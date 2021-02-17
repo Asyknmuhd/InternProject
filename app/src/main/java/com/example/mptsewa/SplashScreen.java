aea3197a9e1af3dcf54a77f24f94ece0829c10ef
DESKTOP-KQ0GB22\ADMIN <nurasyikinmuhamad24@gmail.com>
2021-02-18 00:22:47 +0800

    gambar and tukar ui sikit

---
+++ app/src/main/java/com/example/mptsewa/SplashScreen.java
@@ -0,0 +1,50 @@
+package com.example.mptsewa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//
+//        setContentView(R.layout.activity_splash_screen);
+//        getSupportActionBar().hide();
+//        LogoLauncher logoLauncher = new LogoLauncher();
+//        logoLauncher.start();
+    }
+
+//    private class LogoLauncher extends Thread{
+//        public void run(){
+//            try{
+//                sleep(1000 * SLEEP_TIMER);
+//            }catch(InterruptedException e){
+//                e.printStackTrace();
+//            }
+//
+//            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+//            startActivity(intent);
+//            SplashScreen.this.finish();
+//        }
+//    }
+}
